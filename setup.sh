ef100cc4f11fc303604b616ad45a0a086e1fde46
Jason Kulatunga <jk17@ualberta.ca>
2013-11-17 16:19:15 -0800

    init the install script.

---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+###############################################################################
+#install Greyhole
+
+sudo sh -c 'echo "deb http://www.greyhole.net/releases/deb stable main" > /etc/apt/sources.list.d/greyhole.list'
+curl -s http://www.greyhole.net/releases/deb/greyhole-debsig.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install greyhole
+
+#configure Greyhole
+#https://raw.github.com/gboudreau/Greyhole/master/USAGE
+
+###############################################################################
+#install Docker
+
+#Ubuntu Raring already comes with the 3.8 kernel, so we don’t need to install it. However, not all systems have AUFS filesystem support enabled, so we need to install it.
+sudo apt-get update
+sudo apt-get install linux-image-extra-`uname -r`
+
+# Add the Docker repository key to your local keychain
+# using apt-key finger you can check the fingerprint matches 36A1 D786 9245 C895 0F96 6E92 D857 6A8B A88D 21E9
+sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+
+# Add the Docker repository to your apt sources list.
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\
+> /etc/apt/sources.list.d/docker.list"
+
+# update
+sudo apt-get update
+
+# install
+sudo apt-get install lxc-docker
+
+# verify that it worked.
+sudo docker version
+
+###############################################################################
+#setup the env variables for the docker containers
+
+DELUGE_BRIDGE_IP=192.168.1.1
+PLEX_BRIDGE_IP=192.168.1.2
+SICKBEARD_BRIDGE_IP=192.168.1.3
+COUCHPOTATO_BRIDGE_IP=192.168.1.4
+MEDIAFRONTPAGE_BRIDGE_IP = 192.168.1.5
+
+
+###############################################################################
+#build the docker containers
+sudo docker build -rm -t deluge:deluge github.com/AnalogJ/docker-deluge
+
+#sudo docker build -rm -t plex:plex github.com/AnalogJ/docker-plex
+
+sudo docker build -rm -t sickbeard:sickbeard github.com/AnalogJ/docker-sickbeard
+
+#sudo docker build -rm -t couchpotato:couchpotato github.com/AnalogJ/docker-couchpotato
+
+#sudo docker build -rm -t mediafrontpage:mediafrontpage github.com/AnalogJ/docker-mediafrontpage
+
+###############################################################################
+#run the docker containers
+# note: the order is important, as some contianers depend on other containers.
+
+DELUGE=$(docker run -p 54323:54323 -i -t deluge)
+
+SICKBEARD=$(docker run -p 54322:54322 -i -t sickbeard)
+
+###############################################################################
+#setup pipework to allow inter-container communication
+pipework/pipework br1 $DELUGE $DELUGE_BRIDGE_IP/24
+pipework/pipework br2 $SICKBEARD $SICKBEARD_BRIDGE_IP/24
+
